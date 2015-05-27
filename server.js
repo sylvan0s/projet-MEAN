bcd0ba9e5db9a9ea96d961637f93779fafc01502
mrldm <benlal_m@etna-alternance.net>
2015-05-27 14:46:20 +0200

    dernières modif 'ne pas toucher à server.js"

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 // les modules========================
     var express  = require('express');
     var app      = express();                               
-    //var mongoose = require('mongoose');                    
+    var mongoose = require('mongoose');                    
     var morgan = require('morgan');             
     var bodyParser = require('body-parser');    
     var methodOverride = require('method-override'); 
 
     // configuration =================
+   
 
-<<<<<<< Updated upstream
-    //mongoose.connect('mongodb://node:nodeuser@localhost');     
-=======
     mongoose.connect('mongodb://node:nodeuser@localhost/blog');     
->>>>>>> Stashed changes
+
 
     app.use(express.static(__dirname + '/public'));                 
     app.use(morgan('dev'));                                        
