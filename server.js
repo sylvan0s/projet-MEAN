4187f44c91e62bdb21795d3376f61853e67b7845
mrldm <benlal_m@etna-alternance.net>
2015-05-27 14:41:04 +0200

    motif server.js

--- server.js
+++ server.js
@@ -8,7 +8,11 @@
 
     // configuration =================
 
+<<<<<<< Updated upstream
     //mongoose.connect('mongodb://node:nodeuser@localhost');     
+=======
+    mongoose.connect('mongodb://node:nodeuser@localhost/blog');     
+>>>>>>> Stashed changes
 
     app.use(express.static(__dirname + '/public'));                 
     app.use(morgan('dev'));                                        
@@ -19,4 +23,63 @@
 
     // ouverture du port ======================================
     app.listen(8080);
-    console.log("App listening on port 8080");
+    console.log("Je suis à l'écoute 8080");
+
+    // define model =================
+    var Todo = mongoose.model('article', {
+        text : String
+    });
+
+
+// routes ======================================================================
+    app.use(function(req, res, next) {
+        // se connecter 
+        console.log('il se passe quelque chose.');
+        next();  
+    });
+    // 
+    app.get('/', function(req, res) {
+        res.json({ message: 'hooray! welcome to our api!' });   
+    });
+
+    app.get('/api/articles', function(req, res) {
+
+        Todo.find(function(err, articles) {
+
+            if (err)
+                res.send(err)
+
+            res.json(articles); 
+        });
+    });
+
+    app.post('/api/', function(req, res) {
+
+        Todo.create({
+            text : req.body.text,
+            done : false
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+            Todo.find(function(err, articles) {
+                if (err)
+                    res.send(err)
+                res.json(articles);
+            });
+        });
+
+    });
+
+    app.delete('/api/articles/:articles_id', function(req, res) {
+        Todo.remove({
+            _id : req.params.todo_id
+        }, function(err, todo) {
+            if (err)
+                res.send(err);
+            Todo.find(function(err, articles) {
+                if (err)
+                    res.send(err)
+                res.json(articles);
+            });
+        });
+    });
