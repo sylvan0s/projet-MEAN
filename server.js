4edfb833a1db70a7bb16eccd2a5b21ca4132b57a
mrldm <benlal_m@etna-alternance.net>
2015-05-27 13:19:19 +0200

    config node

---
+++ server.js
@@ -0,0 +1,22 @@
+// les modules========================
+    var express  = require('express');
+    var app      = express();                               
+    var mongoose = require('mongoose');                    
+    var morgan = require('morgan');             
+    var bodyParser = require('body-parser');    
+    var methodOverride = require('method-override'); 
+
+    // configuration =================
+
+    mongoose.connect('mongodb://node:nodeuser@localhost');     
+
+    app.use(express.static(__dirname + '/public'));                 
+    app.use(morgan('dev'));                                        
+    app.use(bodyParser.urlencoded({'extended':'true'}));            
+    app.use(bodyParser.json());                                     
+    app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+    app.use(methodOverride());
+
+    // ouverture du port ======================================
+    app.listen(8080);
+    console.log("App listening on port 8080");
